6d060721d0e2f1b6e8cb5811d2392d32b05d88f2
mahdiASC <mahdi@allstarcode.org>
2018-08-04 06:07:23 -0400

    testing

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 // listen for requests :)
-app.listen(process.env.PORT);
+const port = 3000;
+app.listen(port,() => console.log(`'Example app listening on port: ${port}`));
 
 // init sqlite db
 var fs = require('fs');
@@ -16,19 +17,21 @@ var db = new sqlite3.Database(dbFile);
 const BASE_URL = "http://api.petfinder.com/";
 const API_KEY = process.env.API_KEY;
 
-const createTables = ()=>{
-  db.run("CREATE TABLE 'users' ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `username` TEXT NOT NULL UNIQUE, `salt` TEXT, `password` TEXT NOT NULL )");
-  db.run("CREATE TABLE `shelters` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT, `url` TEXT, `formUrl` TEXT NOT NULL )");
-  db.run("CREATE TABLE `questions` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `shelterId` INTEGER NOT NULL, `formInputName` TEXT NOT NULL, `metaAnswerId` INTEGER NOT NULL );");
-  db.run("CREATE TABLE `metaAnswers` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `inputType` INTEGER DEFAULT 'text' )");
+const addDummyData = function(){
+  db.serialize(function(){
+    
+  });
 }
 
-
-db.serialize(function(){
-  if (!exists) {
-    createTables();
+(()=>{
+  if(!exists){
+    db.run("CREATE TABLE 'users' ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `username` TEXT NOT NULL UNIQUE, `salt` TEXT, `password` TEXT NOT NULL )");
+    db.run("CREATE TABLE `shelters` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT, `url` TEXT, `formUrl` TEXT NOT NULL )");
+    db.run("CREATE TABLE `questions` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `shelterId` INTEGER NOT NULL, `formInputName` TEXT NOT NULL, `metaAnswerId` INTEGER NOT NULL );");
+    db.run("CREATE TABLE `metaAnswers` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `inputType` INTEGER DEFAULT 'text' )");
+    addDummyData();
   }
-});
+})();
 
 // ROUTES
 app.get('/', function(request, response) {
